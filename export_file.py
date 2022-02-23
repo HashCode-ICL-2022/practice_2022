9a5f49c63a5c503c798b52bfc5c43547abb8185f
matthew collins <matthew.collins21@imperial.ac.uk>
2022-02-23 20:17:29 +0000

    export

--- export_file.py
+++ export_file.py
@@ -1,3 +1,8 @@
 def export_file(name, ingredients):
+
+    string = str(len(ingredients))
+    for ingredient in ingredients:
+        string += ' ' + ingredient
+
     f = open('outputs/' + name, "w")
-    f.writelines([len(ingredients)].extend(ingredients))
+    f.writelines(string)
