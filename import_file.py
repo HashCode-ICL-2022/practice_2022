b1eb1664a037fad169e3cc367af1857cf01b33ff
matthew collins <matthew.collins21@imperial.ac.uk>
2022-02-23 19:53:11 +0000

    fixes

--- import_file.py
+++ import_file.py
@@ -1,11 +1,10 @@
-def import_file(filename):
-    with open(file_name, 'r') as file:
+def import_file(file_name):
+    with open('data/' + file_name, 'r') as file:
         # Read first line
         N = int(file.readline())
 
-        print(f"Loading {N} Customers")
+        # print(f"Loading {N} Customers")
         likes = []
-        avg_l = []
         for i in range(N):
             line = file.readline().rstrip().split(" ")
             likes.extend(line[1:])
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+from import_file import import_file
 likes, dislikes = import_file('a_an_example.in.txt')
+
+print(likes)
+print(dislikes)
